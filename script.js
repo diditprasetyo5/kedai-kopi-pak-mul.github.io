4a34387ec4e378e233d021cc3f9f24b78ecc1e22
diditprasetyo5 <diditprasetyo5@gmail.com>
2023-02-19 20:58:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//toggle
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humberger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar side bar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
